303fa34997c9657a2e26f7b189e9fd4c51f4d992
mstaunstrup <staunstrup.m@gmail.com>
2015-10-27 11:30:19 +0100

    2.

---
+++ app/src/main/java/com/example/marianne/ejendomsselskabetbmapp/Inspectionlist.java
@@ -0,0 +1,106 @@
+package com.example.marianne.ejendomsselskabetbmapp;
+
+
+import org.w3c.dom.Text;
+
+import java.util.Date;
+
+public class Inspectionlist {
+
+    private int _id;
+    private Date _date;
+    private String _room; // Skal laves til enum
+    private String _addres; // Skal laves til enum
+    private Boolean _acquisition;
+    private String _taskdescription;
+    private Boolean _scheduled;
+    private Boolean _completed;
+    private String _pictureaddress;
+
+    public Inspectionlist(){}
+
+    public Inspectionlist(String room){
+        this._room = room;
+    }
+    // En fuld constructor dog uden id
+    public Inspectionlist(Date _date, String _room, String _addres, Boolean _acquisition,
+                          String _taskdescription, Boolean _scheduled, Boolean _completed,
+                          String _pictureaddress) {
+        this._date = _date;
+        this._room = _room;
+        this._addres = _addres;
+        this._acquisition = _acquisition;
+        this._taskdescription = _taskdescription;
+        this._scheduled = _scheduled;
+        this._completed = _completed;
+        this._pictureaddress = _pictureaddress;
+    }
+
+    public void set_date(Date _date) {
+        this._date = _date;
+    }
+
+    public void set_room(String _room) {
+        this._room = _room;
+    }
+
+    public void set_addres(String _addres) {
+        this._addres = _addres;
+    }
+
+    public void set_acquisition(Boolean _acquisition) {
+        this._acquisition = _acquisition;
+    }
+
+    public void set_taskdescription(String _taskdescription) {
+        this._taskdescription = _taskdescription;
+    }
+
+    public void set_scheduled(Boolean _scheduled) {
+        this._scheduled = _scheduled;
+    }
+
+    public void set_completed(Boolean _completed) {
+        this._completed = _completed;
+    }
+
+    public void set_pictureaddress(String _pictureaddress) {
+        this._pictureaddress = _pictureaddress;
+    }
+
+    public int get_id() {
+        return _id;
+    }
+
+    public Date get_date() {
+        return _date;
+    }
+
+    public String get_room() {
+        return _room;
+    }
+
+    public String get_addres() {
+        return _addres;
+    }
+
+    public Boolean get_acquisition() {
+        return _acquisition;
+    }
+
+    public String get_taskdescription() {
+        return _taskdescription;
+    }
+
+    public Boolean get_scheduled() {
+        return _scheduled;
+    }
+
+    public Boolean get_completed() {
+        return _completed;
+    }
+
+    public String get_pictureaddress() {
+        return _pictureaddress;
+    }
+}
---
+++ app/src/main/java/com/example/marianne/ejendomsselskabetbmapp/MyDBHandler.java
@@ -0,0 +1,38 @@
+package com.example.marianne.ejendomsselskabetbmapp;
+
+import android.database.DatabaseErrorHandler;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.database.Cursor;
+import android.content.Context;
+import android.content.ContentValues;
+
+public class MyDBHandler extends SQLiteOpenHelper {
+
+    private static final int DATABASE_VERSION = 1;
+    private static final String DATABASE_NAME = "inspectionlist.db";
+    public static final String TABLE_INSPECTIONLIST = "inspectionlist";
+    public static final String COLUMN_ID = "_id";
+    public static final String COLUMN_DATE = "date";
+    public static final String COLUMN_ROOM = "room";
+    public static final String COLUMN_ADDRESS = "address";
+    public static final String COLUMN_ACQUISITION = "acquisition";
+    public static final String COLUMN_TASKDESCRIPTION = "taskdescription";
+    public static final String COLUMN_SCHEDULED = "scheduled";
+    public static final String COLUMN_COMPLETED = "completed";
+    public static final String COLUMN_PICTUREADDRESS = "pictureaddress";
+
+    public MyDBHandler(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+}
