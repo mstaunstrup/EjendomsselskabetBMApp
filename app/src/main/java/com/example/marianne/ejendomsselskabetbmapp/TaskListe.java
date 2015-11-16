a579e1e665d3f402934d97e31a97501f08b0c452
msuhl <gate.nielsen@gmail.com>
2015-11-16 13:39:58 +0100

    doubletask v5

--- app/src/main/java/com/example/marianne/ejendomsselskabetbmapp/TaskListe.java
+++ app/src/main/java/com/example/marianne/ejendomsselskabetbmapp/TaskListe.java
@@ -1,22 +1,17 @@
 package com.example.marianne.ejendomsselskabetbmapp;
 
-import android.content.Intent;
 import android.database.Cursor;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.SimpleCursorAdapter;
 
 public class TaskListe extends AppCompatActivity {
 
     MyDBHandler myDB;
-    public final static String NAME_EXTRA = "com.example.marianne.ejendomsselskabetbmapp.COLUMN_ID";
-    SimpleCursorAdapter myCursorAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -55,36 +50,11 @@ public class TaskListe extends AppCompatActivity {
         Cursor cursor = myDB.GetAllRows();
         String[] fromFieldNames = new String[]{MyDBHandler.COLUMN_TASKDESCRIPTION, MyDBHandler.COLUMN_DATE, MyDBHandler.COLUMN_ROOM};
         int[] toViewID = new int[]{R.id.txtTask, R.id.txtdate, R.id.txtRoom};
-
+        SimpleCursorAdapter myCursorAdapter;
         myCursorAdapter = new SimpleCursorAdapter(getBaseContext(), R.layout.item_layout2, cursor, fromFieldNames, toViewID,0);
 
         ListView taskList = (ListView)findViewById(R.id.lsvTaskDate);
 
         taskList.setAdapter(myCursorAdapter);
-
-        taskList.setOnItemClickListener(
-                new AdapterView.OnItemClickListener() {
-                    @Override
-                    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                        Intent singleTask = new Intent(TaskListe.this, SingleTask.class);
-                        // henter cursoren, så jeg kan bruge den
-                        Cursor c = ((SimpleCursorAdapter)myCursorAdapter).getCursor();
-//flytter cursor til den valgte position
-                        c.moveToPosition(position);
-// henter string fra column 5 i db
-                        String taskDescription = c.getString(5);
-
-
-                        Log.d("Sendt til singleTask ", taskDescription);
-                        singleTask.putExtra(NAME_EXTRA, taskDescription);
-                        Log.d("Sendt til singleTask ", String.valueOf(id));
-                        //singleTask.putExtra(NAME_EXTRA, String.valueOf(id));
-
-                        startActivity(singleTask);
-                        //String item = String.valueOf(parent.getItemAtPosition(position));
-                        //Toast.makeText(TaskListe.this, item, Toast.LENGTH_LONG).show();
-                    }
-                }
-        );
     }
 }
